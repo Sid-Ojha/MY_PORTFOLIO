f320ba26a7b055319daaeaada0088cb369c9665e
Sid-Ojha <90849139+Sid-Ojha@users.noreply.github.com>
2023-05-03 04:29:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+$(document).ready(function () {
+    $('#menu').click(function () {
+        $(this).toggleClass('fa-times');
+        $('header').toggleClass('toggle');
+    }); I
+    $(window).on('scroll load', function () {
+        $('#menu').removeClass('fa-times');
+        $('header').removeClass('toggle');
+
+
+    });
+
+
+    $('a[href*="#"]').on('click', function (e) {
+        e.preventDefault();
+        $('html,body').animate({
+            scrollTop: $($(this).attr('href')).offset().top,
+        },
+            500,
+            'linear'
+
+        );
+    });
+});
+
+
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  // Close the dropdown if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
+
+
